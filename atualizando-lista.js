e0dd38292c92b5df3d97408652e868a4938b43f7
Matheus Shimanoe Torresi <36981198+JackShima@users.noreply.github.com>
2023-04-23 21:53:00 -0300

    Add files via upload

---
+++ atualizando-lista.js
@@ -0,0 +1,13 @@
+// const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"]
+
+// nomes.splice(1, 2, "Rodrigo"); //remove Ana e Caio
+// //nomes.push("Rodrigo");
+
+// console.log(nomes);
+
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(0,1,'🐠')
+animaisDoAquario.splice(2,3,'🐟')
+
+console.log(animaisDoAquario)
