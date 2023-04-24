88211ce3b587e7e3a7c0179c5dfef66576ccbf91
Matheus Torresi <matheus.torresi@C02FH1BTML7J.local>
2023-04-23 21:10:50 -0300

    Oque são Arrays

---
+++ media.js
@@ -0,0 +1,20 @@
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+
+// const media = (nota1 + nota2 + nota3 + nota4) / 4
+
+// console.log(media)
+
+const notas = [10, 6.5, 8, 7.5];
+
+let media = 0; //= (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
+console.log(media)
+
+// for (let i = 0; i < notas.length; i++) {
+//     const soma =+ notas[i];
+//     return media = soma;
+// }
+
+// console.log(media/notas.length)
